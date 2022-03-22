e921ebd83eb7f0861a597f01d35c368064d840ba
robertoleiva <47877340+robertoleiva@users.noreply.github.com>
2022-03-21 21:20:05 -0600

    nuevo

--- lib/resorte/Fijo.dart
+++ lib/resorte/Fijo.dart
@@ -10,6 +10,77 @@ List articulos = [
         "Silla de comedor "
   },
 
+  {
+    "img": "assets/images/Grupo2.png",
+    "titulo": "Silla #2",
+    "precio": 27.00,
+    "descripcion":
+        "Silla de jardin "
+  },
+  {
+    "img": "assets/images/Grupo3.png",
+    "titulo": "Silla #3",
+    "precio": 28.00,
+    "descripcion":
+        "Silla playera"
+  },
+
+  {
+    "img": "assets/images/Grupo4.png",
+    "titulo": "Silla #4",
+    "precio": 29.00,
+    "descripcion":
+        "Silla de sala "
+  },
+
+  {
+    "img": "assets/images/GrupoC1.png",
+    "titulo": "Cuna para bebe ",
+    "precio": 99.00,
+    "descripcion":
+        "Cuna doble colchon"
+  },
+
+  {
+    "img": "assets/images/GrupoC2.png",
+    "titulo": "Cuna para bebe ",
+    "precio": 125.00,
+    "descripcion":
+        "Cuna doble colchon"
+  },
+
+  {
+    "img": "assets/images/GrupoC3.png",
+    "titulo": "Cuna para bebe ",
+    "precio": 115.00,
+    "descripcion":
+        "Cuna doble colchon"
+  },
+
+  {
+    "img": "assets/images/GrupoS1.png",
+    "titulo": "Sillon familiar ",
+    "precio": 234.00,
+    "descripcion":
+        "Sillon de sala 3 personas"
+  },
+
+  {
+    "img": "assets/images/GrupoS2.png",
+    "titulo": "Sillon de Sala",
+    "precio": 167.00,
+    "descripcion":
+        "Sillon de sala 2 personas"
+  },
+  {
+    "img": "assets/images/GrupoS3.png",
+    "titulo": "Sillon Individual",
+    "precio": 191.00,
+    "descripcion":
+        "Sillon creativo"
+  },
+
+
 
 
 
