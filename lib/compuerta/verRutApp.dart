5aa18694a4c240530f887e2e77f6d25f276e9d72
robertoleiva <47877340+robertoleiva@users.noreply.github.com>
2022-03-21 21:41:06 -0600

    pasos

---
+++ lib/compuerta/verRutApp.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:parcial2/paginas/Describir.dart';
+import 'package:parcial2/resorte/Fijo.dart';
+
+
+class verRutApp extends StatefulWidget {
+  verRutApp({Key? key}) : super(key: key);
+
+  @override
+  State<verRutApp> createState() => _verRutAppState();
+}
+
+class _verRutAppState extends State<verRutApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/paginas/RutApp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:parcial2/compuerta/verRutApp.dart';
+import 'package:parcial2/tempora/AttSilla.dart';
+
+
+
+class RutApp extends StatefulWidget {
+  RutApp({Key? key}) : super(key: key);
+
+  @override
+  State<RutApp> createState() => _RutAppState();
+}
+
+class _RutAppState extends State<RutApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
