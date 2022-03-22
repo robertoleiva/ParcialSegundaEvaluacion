af5b7a1e1d6fa98e867d9249b8e02104d9559ffc
robertoleiva <47877340+robertoleiva@users.noreply.github.com>
2022-03-21 21:03:16 -0600

    si

--- lib/resorte/Fijo.dart
+++ lib/resorte/Fijo.dart
@@ -1,3 +1,17 @@
 List categorias = ["Muebles", "Cuadros", "Camas", "Sillas", "Cunas"];
 
-List articulos = [];
+List articulos = [
+
+{
+    "img": "assets/images/Grupo1.png",
+    "titulo": "Silla #1",
+    "precio": 26.00,
+    "descripcion":
+        "Silla de comedor "
+  },
+
+
+
+
+
+];
